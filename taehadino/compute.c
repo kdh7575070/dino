b120986553e15103ef4d4e207675113f125e1be0
Seungsin <62578738+Seungsin@users.noreply.github.com>
2020-12-14 21:39:06 +0900

    Update compute.c
    buffer compute+./compute2 합친것!

--- taehadino/compute.c
+++ taehadino/compute.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <string.h>
 #define MAX_QUEUE_SIZE 6
 
 struct user {
@@ -20,14 +21,12 @@ obstacle *queue;
 int rear, front;
 
 void startEngine(int highScore, struct user firstUser);
-int computePrize(int score, int usedPrize);
 void endGame(int score, int highScore, int diY, int diX, struct user firstUser);
 void showDinasour(int diY, int diX);
 void startMenu();
 int computeTime(int delayTime);
 int checkGame(int y, int x, int diY, int diX);
-
-
+int showScoreBoard();
 int isObstacle;
 void qinitialize();
 int isEmpty();
@@ -90,16 +89,42 @@ int checkGame(int y, int x, int diY, int diX) {
 	return 1;
 }
 
+int showScoreBoard()
+{
+	int highScore, scores;
+	char name[100];
+	int count=0;
+	int maxX = getmaxx(stdscr)/2;
+	int maxY = getmaxy(stdscr)/2;
+	 // Read high score from file
+	FILE *highScoreFile;
+	highScoreFile = fopen("./highScore.txt", "r");
+
+	mvprintw(maxY-5, maxX-15, "          << Score Board! >>\n");
+	mvprintw(maxY-3, maxX-15, "%s %30s\n", "Score", "name");
+	fscanf(highScoreFile, "%d %s", &scores, name);
+	while(!feof(highScoreFile)){
+		if(count==0) highScore = scores;
+		mvprintw(maxY+count-1, maxX-15, "%4d %30s", scores, name);
+		fscanf(highScoreFile, "%d %s", &scores, name);
+		count++;
+		if(count==10) break;
+	}
+	fclose(highScoreFile);
+	getstr(name);
+	return highScore;
+}
+
 // Make game faster
 int computeTime(int delayTime) {
 	if (delayTime >= 250000) {
-		delayTime -= 900;
+		delayTime -= 10000;
 	}
 	else if (delayTime >= 200000) {
-		delayTime -= 600;
+		delayTime -= 5000;
 	}
 	else {
-		delayTime -= 200;
+		delayTime -= 10;
 	}
 	return delayTime;
 }
@@ -107,12 +132,8 @@ int computeTime(int delayTime) {
 // The very first menu
 void startMenu() {
 	struct user firstUser;
-	int highScore;
-    // Read high score from file
-	FILE *highScoreFile;
-	highScoreFile = fopen("./highScore.txt", "r");
-	fscanf(highScoreFile, "%d", &highScore);
-	fclose(highScoreFile);
+	int highScore, scores;
+	
 	int maxX = getmaxx(stdscr)/2;
 	int maxY = getmaxy(stdscr)/2;
 	init_pair(3,COLOR_GREEN,COLOR_BLACK);
@@ -120,16 +141,20 @@ void startMenu() {
 	showTrex(maxY, maxX);
 	attroff(COLOR_PAIR(3));
     // Get information from user
-	mvprintw(maxY+1, maxX-28, "Write inputs and press Enter to start Game.");
-    mvprintw(maxY+2, maxX-26, "When you had prize, fire it with 'k' key!");
-	mvprintw(maxY+3, maxX-21, "You can jump with space key!");
-	mvprintw(maxY+4, maxX-15, "Name: ");
+	mvprintw(maxY+2, maxX-24, "Write inputs and press Enter to start Game.");
+    mvprintw(maxY+3, maxX-22, "In case you wanna quit, put CTRL+C !");
+	mvprintw(maxY+4, maxX-20, "You can jump with space key!");
+	mvprintw(maxY+6, maxX-12, "Name: ");
 	getstr(firstUser.name);
-	mvprintw(maxY+5, maxX-15, "Last name: ");
+	mvprintw(maxY+7, maxX-12, "Last name: ");
 	getstr(firstUser.lastName);
-	mvprintw(maxY+6, maxX-15, "Age: ");
+	mvprintw(maxY+8, maxX-12, "Age: ");
 	getstr(firstUser.age);
 	noecho();
+
+	clear();
+	highScore = showScoreBoard();
+	
 	startEngine(highScore, firstUser);
 }
 
@@ -150,25 +175,47 @@ void showDinasour(int diY, int diX) {
 void endGame(int score, int highScore, int diY, int diX, struct user firstUser) {
 	nodelay(stdscr, FALSE);
 	init_pair(2,COLOR_RED,COLOR_BLACK);
+	int scores;
+	char name[100];
+	char write[110];
+	char writing[10][110];
+	int iswrite=0;
     // Save high score
-	if (score > highScore) {
-		highScore = score;
-		FILE *highScoreFile;
-		highScoreFile = fopen("./highScore.txt", "w");
-		fprintf(highScoreFile, "%d", highScore);
-		fclose(highScoreFile);
+	FILE *highScoreFile;
+	highScoreFile = fopen("./highScore.txt", "r");
+	int count=0;
+	fscanf(highScoreFile, "%d %s", &scores, name);
+	while(!feof(highScoreFile)){
+		if(scores<score&&iswrite==0){
+			if(!strcmp(firstUser.name,""))strcpy(firstUser.name, "NULL");
+			sprintf(write, "%d %s\n", score, firstUser.name);
+			strcpy(writing[count++], write);
+			iswrite=1;
+			if(count==10) break;
+		}
+		sprintf(write, "%d %s\n", scores, name);
+		strcpy(writing[count++], write);
+		if(count==10) break;
+		fscanf(highScoreFile, "%d %s", &scores, name);
 	}
+	fclose(highScoreFile);
+	highScoreFile = fopen("./highScore.txt", "w");
+	for(int i=0;i<count;i++) fprintf(highScoreFile, "%s", writing[i]);
+	fclose(highScoreFile);
+
 	int maxX = getmaxx(stdscr)/2;
 	int maxY = getmaxy(stdscr)/2;
 	attron(COLOR_PAIR(2));
 	showLoss(maxY, maxX);
 	char keyToExit = getch();
     // Exit or reset game
-	if (keyToExit == 'r') {
+	if (keyToExit == 'r' || keyToExit == 'R') {
 		attroff(COLOR_PAIR(2));
 		startEngine(highScore, firstUser);
 	}
-	else if (keyToExit == 'q') {
+	else if (keyToExit == 'q' || keyToExit == 'Q') {
+		clear();
+		showScoreBoard();
 		return;
 	}
 	else {
@@ -176,31 +223,24 @@ void endGame(int score, int highScore, int diY, int diX, struct user firstUser) 
 	}
 }
 
-// Give user the arrow
-int computePrize(int score, int usedPrize) {
-	if (score >= 20 && score <= 40 && usedPrize == 0) {
-		return 1;
-	}
-	else if (score >= 60 && score <= 80 && usedPrize <= 1) {
-		return 1;
-	}
-	else if (score >= 100 && score <= 120 && usedPrize <= 2) {
-		return 1;
-	}	
-	return 0;
-}
-
 // The main engine!
-void startEngine(int highScore, struct user firstUser) {
-    srand(time(NULL));
-	int x, y, diX=10, prize=0, usedPrize=0, score=0, delayTime = 300000
-    , gameStatus=1, cactusNum=0;
-	int maxX=getmaxx(stdscr);
-	x = maxX-20;
-	y = getmaxy(stdscr)-6;
+void startEngine(int highScore, struct user firstUser)
+{
+	srand(time(NULL));
+	int x, y, diX = 10, score = 0, delayTime = 300000, gameStatus = 1, cactusNum = 0;
+	int maxX = getmaxx(stdscr);
+	x = maxX - 20;
+	y = getmaxy(stdscr) - 6;
 	int diY = y;
-	char userInput, userInput2;
-	char beforInput=' ';
+	char userInput;
+	clear();
+	nodelay(stdscr, TRUE);
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	init_pair(4, COLOR_BLUE, COLOR_BLACK);
+	bool isBottom = true;
+	bool isJumping = false;
+	bool beforeInput_jump = false;
+
 	obstacle cactus;
 	queue = Queue; // obstacle queue
 	qinitialize(); // init queue
@@ -208,75 +248,99 @@ void startEngine(int highScore, struct user firstUser) {
 	cactus.y = y; 
 	cactus.type = cactusNum;
 	addq(cactus);  // add cactus before while
-	
-    clear();
-	nodelay(stdscr, TRUE);
-  	init_pair(1,COLOR_WHITE,COLOR_BLACK);
-	init_pair(4,COLOR_BLUE,COLOR_BLACK);
-	while (gameStatus == 1) {
-		userInput2 = getch();
-		userInput=userInput2;
-		if(userInput2==beforInput){
-			userInput='p';
-		}
-		beforInput=userInput2;
+
+	while (gameStatus == 1)
+	{
+		userInput = getch();
 		tcflush(0, TCIFLUSH);
+
 		// Show day or night
-		if (((score/100))%2){//100의자리대가짝수일때
-            clear();
+		if (((score / 100)) % 2)
+		{ //100의자리대가짝수일때
+			clear();
 			attron(COLOR_PAIR(3));
-			moon(10, (maxX/2)-10);			
+			moon(10, (maxX / 2) - 10);
 		}
-		else {
+		else
+		{
 			attron(COLOR_PAIR(1));
-			sun(10, (maxX/2)-10);			
+			sun(10, (maxX / 2) - 10);
 		}
 
 		score++;
-        // Show informations
+		// Show informations
 		mvprintw(1, 6, "%s %s", firstUser.name, firstUser.lastName);
-		mvprintw(1, getmaxx(stdscr)-9, "%d:%d", highScore, score);
+		mvprintw(1, getmaxx(stdscr) - 9, "%d:%d", highScore, score);
 
 		// ----------
 		box(stdscr, ACS_VLINE, ACS_HLINE);
+
 		//for clearing screen
 		clearDinasourUp(diY, diX);
-		/* random obstacle */
 		isObstacle = rand() % 10; 		// create obstacle or not
 		if( isObstacle == 1 ){	   		// probability of creating obstacle: 10%
 			cactusNum = rand() % 3; 	// decide the type of cactus
 			cactus.x = getmaxx(stdscr)-20; // initial x-axis position of cactus
 			cactus.y = y;			// initial y-axis position of cactus
 			cactus.type = cactusNum;	// type of cactus
-			addq(cactus);			// add cactus to queue
+			addq(cactus);
 		}
-        // if input is equal to ' ' then jump
-		if (userInput == ' ') {
+		// if input is equal to ' ' then jump
+		if (userInput == ' ' && isBottom && !isJumping)
+		{
+			isJumping = true;
+			isBottom = false;
+			beforeInput_jump = true;
+		}
+
+		if (isJumping)
+		{
 			diY -= 7;
 		}
-		showDinasour(diY, diX);
-		if (userInput == ' ') {
-			clearDinasourDown(diY, diX);		
+		else if (beforeInput_jump)
+		{
+			beforeInput_jump = false;
 		}
-		obstacle first = queue[ (front+1)%MAX_QUEUE_SIZE ];   // closest obstacle from dinosaur
-		showObstacle();					// show all cactus in queue
-		
-		gameStatus = checkGame(first.y, first.x, diY, diX);
-        // Bring back dinosaur
-		if (userInput == ' ') {
+		else
+		{
 			diY += 7;
 		}
-		mvhline(y+1, 1, '-', getmaxx(stdscr)-3);
+
+		if (diY >= y)
+		{
+			diY = y;
+			isBottom = true;
+		}
+
+		if (isJumping)
+		{
+			clearDinasourDown(diY, diX);
+			isJumping = false;
+		}
+		else clearDinasourUp(diY, diX);
+		if (diY <= 7)
+		{
+			isJumping = false;
+		}
+
+		obstacle first = queue[ (front+1)%MAX_QUEUE_SIZE ];   // closest obstacle from dinosaur
+		showObstacle();					// show all cactus in queue
+
+		gameStatus = checkGame(first.y, first.x, diY, diX);
+		// Bring back dinosaur
+
+		mvhline(y + 1, 1, '-', getmaxx(stdscr) - 3);
 		updateObstacle();
-        refresh();
-        clearCactus1(y, x);
+		showDinasour(diY, diX);
 		refresh();
-        usleep(delayTime);
-        x -= 7;
-        delayTime = computeTime(delayTime);
+		// clearCactus1(y, x);
+		// refresh();
+		usleep(delayTime);
+		x -= 7;
+		delayTime = computeTime(delayTime);
 		userInput = 'q';
 	}
-    // When code reaches here, means that user is loss
+	// When code reaches here, means that user is loss
 	endGame(score, highScore, diY, diX, firstUser);
 	attroff(COLOR_PAIR(1));
 }
